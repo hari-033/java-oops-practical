50d58688b7f5eb0950ab0c28696e92c0b8d71e28
hari-033 <2420030244@klh.edu.in>
2025-07-30 10:45:32 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+package oops;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			System.out.println("count: " + i);
+		}
+	}
+}
---
+++ Relationaloperators.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class Relationaloperators {
+	public static void main(String[] args) {
+		int x = 10, y = 20;
+		System.out.println(x > y); // false
+		System.out.println(x < y); // true
+		System.out.println(x == y); // false
+		}
+}
